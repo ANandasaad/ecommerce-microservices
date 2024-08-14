016a4872578a49566e985e7ca60e62973e3968c5
Anand kushwaha <anandkushwaha@Anands-Mac-mini.lan>
2024-08-14 20:31:13 +0530

    nats

--- nats/src/publisher.ts
+++ nats/src/publisher.ts
@@ -1,19 +1,30 @@
 import nats from "node-nats-streaming";
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 console.clear();
 const stan = nats.connect("ticketing", "abc", {
   url: "http://localhost:4222",
 });
 
-stan.on("connect", () => {
+stan.on("connect", async () => {
   console.log("Publisher connected to NATS");
 
-  const data = JSON.stringify({
-    id: "123",
-    title: "Hello",
-    price: 40,
-  });
+  // const data = JSON.stringify({
+  //   id: "123",
+  //   title: "Hello",
+  //   price: 40,
+  // });
 
-  stan.publish("ticket:created", data, () => {
-    console.log("Event published");
-  });
+  // stan.publish("ticket:created", data, () => {
+  //   console.log("Event published");
+  // });
+  const publisher = new TicketCreatedPublisher(stan);
+  try {
+    await publisher.publish({
+      id: "123",
+      title: "Hello",
+      price: 40,
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
