b66a46a1e9efe316e31da4a21bca0bdd12f037ee
Anand kushwaha <anandkushwaha@Anands-Mac-mini.lan>
2024-08-22 21:27:12 +0530

    payment setup

---
+++ payment/src/app.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import "express-async-errors";
+import paymentRoutes from "./routes/payment.routes";
+import { currentUser, errorHandler } from "@akticketorg/commondir";
+import { NotFoundError } from "@akticketorg/commondir";
+import cookieSession from "cookie-session";
+const app = express();
+app.set("trust proxy", true);
+app.use(express.json());
+app.use(
+  cookieSession({
+    secure: process.env.NODE_ENV !== "test",
+    signed: false,
+  })
+);
+app.use(currentUser);
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/paymnet", paymentRoutes);
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
+
+export { app };
