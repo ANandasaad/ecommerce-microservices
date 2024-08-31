3de9161cd66d242e93fdfc9ea1721524b27924d7
Anand kushwaha <anandkushwaha@Anands-Mac-mini.lan>
2024-08-31 22:15:14 +0530

    added startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import { NotFound } from "http-errors";
 import { app } from "./app";
 const start = async () => {
+  console.log("Starting app...");
   if (!process.env.JWT_KEY) {
     throw new NotFound("JWT_KEY is not found");
   }
